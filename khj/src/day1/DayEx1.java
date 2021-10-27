c68d74acef6ac5b2e4ca6087309addd89bbdfc8b
HyeokJin93 <hyeokji93@gmail.com>
2021-10-27 17:31:41 +0900

    1-6 : 변수 선언 문제 및 답

---
+++ khj/src/day1/DayEx1.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class DayEx1 {
+
+	public static void main(String[] args) {
+	/* 핸드폰 번호를 - 없이 저장하여 관리하려 한다. 알맞은 변수를 선언하고
+	 * 초기값으로 01012345678을 저장하여 확인해보세요.
+	 **/
+		String phoneNumber= "01012345671"; 
+		System.out.println(phoneNumber);
+		
+				
+				
+	}
+
+}
