d350cd230d54f0bad27c7d23998b4354e931b744
WaitingDW <bestoboist23@gmail.com>
2021-11-23 17:21:53 +0900

    DB 2일차 : insert 예제

---
+++ 3. 데이터베이스/2일차 데이터 생성 예제.sql
@@ -0,0 +1,12 @@
+ -- insert into student values('2021160123', 'hong', 'hong', '홍길동', '020101-3456789');
+-- id : hong123, pw : pw123으로 하려고 했으나 순서를 착각해서 잘못입력했더니 원하지 않은 결과가 발생
+-- insert into student values('2021160124', 'hong1234', 'pw123', '황길동', '020505-3123456');
+-- insert into student(st_num, st_id, st_pw, st_name, st_reg_num)
+-- values('2021160001','kang','kang','강감찬','021225-3654321');
+-- id :kimid, pw: pwkim으로 하기 위해 속성 순서를 st_pw, st_id순으로 바꿔서 원하는 결과가 나옴
+-- insert into student(st_num, st_id, st_pw, st_name, st_reg_num)
+-- values('2021160001','kang','kang','강감찬','021225-3654321');
+-- INSERT INTO `university_khj`.`student` (`st_num`, `st_id`, `st_pw`, `st_name`, `st_reg_num`) VALUES ('2021160123', 'hong', 'hong', '홍길동', '020101-3456789');
+-- insert into student values('021123005', 'park', 'parkpw', '박철수', '021221-4443332'),
+--					('2021123025', 'parksu', 'parkpw', '박수철', '021224-4443331');
+SELECT * FROM university_khj.student;
