f9f808ec1ad45a87e6c472ad3c93c0af36541213
WaitingDW <bestoboist23@gmail.com>
2022-02-08 11:49:16 +0900

    스프링 : ajax를 이용한 카테고리 선택 예제

--- spring_khj/src/main/java/kr/green/spring/dao/MemberDAO.java
+++ spring_khj/src/main/java/kr/green/spring/dao/MemberDAO.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Param;
 
+import kr.green.spring.vo.MainCategoryVO;
 import kr.green.spring.vo.MemberVO;
+import kr.green.spring.vo.MiddleCategoryVO;
+import kr.green.spring.vo.SubCategoryVO;
 
 public interface MemberDAO {
 	// select문을 씀 - select를 제외한 나머지는 리턴타입이 없음.	
@@ -16,4 +19,7 @@ public interface MemberDAO {
 	List<MemberVO> selectMemberList();
 	void updateAutoLogin(@Param("user")MemberVO user);
 	MemberVO selectMemberBySessionId(@Param("me_session_id")String me_session_id);
+	List<MainCategoryVO> selectMainCategory();
+	List<MiddleCategoryVO> selectMiddleCategory(@Param("mi_ma_num")Integer mi_ma_num);
+	List<SubCategoryVO> selectSubCategory(@Param("su_mi_num")Integer su_mi_num);
 }
